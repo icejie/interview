7dc467e175bea729d1af390e7afaf90af0b3fa26
fly <844251288@qq.com>
2017-04-05 10:05:26 +0800

    add

--- app.js
+++ app.js
@@ -1,31 +1,64 @@
-var express=require('express');
-var app = express();
-var router = require('./routers/module.js');
-var hbs  = require('express-handlebars');
+// var express=require('express');
+// var app = express();
+// var router = require('./routers/module.js');
+// var hbs  = require('express-handlebars');
 
-app.set('view engine', 'hbs'); //使用什么模版引擎
-app.set('views',__dirname + '/views');  //模板放在哪里
-
-// app.engine('html',function(){  //注册模板引擎
-// 	console.log(1);
-//     res.render('home',{
-//     	'title':'jhjkhjhjhjhhjhj'
-//     });
-// }); 
+// app.set('view engine', 'hbs'); //使用什么模版引擎
+// app.set('views',__dirname + '/view');  //模板放在哪里
 
 
 
-var app = express();
-app.listen(3001);
+// app.engine('html', function(a,b,c){//注册模板引擎
+// 	console.log('注册成功');
+// });   
+
+// var app = express();
+// app.listen(3001);
 
 
 
-// app.get('/', function(req, res){
-//     res.render('index', {title: 'hbs demo', author: 'chyingp'});
-// });
+// // app.get('/', function (req, res) {
+// // 	console.log(1);
+// //     res.render('main',{
+// //     	'title':'jhjkhjhjhjhhjhj'
+// //     });
+// // });
 
 
-app.set('one','news');
-console.log(app.get('one'));
-app.use(router);
-app.use(express.static( __dirname +'/pulic'));
+
+
+
+
+
+
+// app.set('one','one');
+// console.log(app.get('one'));
+// app.use(router);
+// app.use(express.static( __dirname +'/view'));
+
+var express = require('express'),
+    epresshbs = require('express-handlebars'),
+    path = require('path'),
+    app = express();
+
+var hbs = epresshbs.create({
+    defaultLayout:'main',
+    extname:".hbs",
+    partialsDir:"views/"
+})
+
+
+app.engine('hbs',hbs.engine);
+app.set('view engine', 'hbs');  // 用hbs作为模版引擎
+app.set('views',path.join(__dirname ,'/views')); // 模版所在路径
+
+app.get('/', function(req, res){
+    res.render('index', {
+        title: 'hbs demo', 
+        author: 'chyingp',
+        message:"hello word"
+    });
+});
+
+
+app.listen(3000);   
